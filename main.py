f8f54eba269caecea2704ee3152385730b8ce4c6
unknown <tariel4ik@gmail.com>
2024-11-12 16:05:27 +0300

    sample

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
-from flask import Flask, render_template
+import os
+import telebot
+from flask import Flask, request
 
-app = Flask(__name__, template_folder='.')
+TOKEN = '1810242195:AAFfImqq9mPT81rs3ApL74eUob5u8A9oxb8'
+APP_URL = f'https://vitaljaheroku.herokuapp.com/{TOKEN}'
+bot = telebot.TeleBot(TOKEN)
+server = Flask(__name__)
 
 
-@app.route("/")
-def web():
-    return render_template('index.html')
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port='80')
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo(message):
+    bot.reply_to(message, message.text)
+
+
+@server.route('/' + TOKEN, methods=['POST'])
+def get_message():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return '!', 200
+
+
+@server.route('/')
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url=APP_URL)
+    return '!', 200
+
+
+if __name__ == '__main__':
+    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
