e668101b8fc2119bedcbd1abab74c4c0f2f4e683
unknown <tariel4ik@gmail.com>
2024-11-12 15:14:28 +0300

    sample

--- main.py
+++ main.py
@@ -1,38 +1,12 @@
-import os
-import telebot
-from flask import Flask, request
+from flask import Flask, render_template
 
-TOKEN = '7580167697:AAGQhpoMI4Oq-WpulwP5uPMOz_edLlpWJ8Q'
-APP_URL = f'https://my-app-game-ef1ec4b42519.herokuapp.com/{TOKEN}'
-bot = telebot.TeleBot(TOKEN)
-server = Flask(__name__)
+app = Flask(__name__, template_folder='.')
 
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.reply_to(message, 'Hello, ' + message.from_user.first_name.getPhotos())
+@app.route("/")
+def web():
+    return render_template('index.html')
 
 
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo(message):
-    bot.reply_to(message, message.text)
-
-
-
-@server.route('/' + TOKEN, methods=['POST'])
-def get_message():
-    json_string = request.get_data().decode('utf-8')
-    update = telebot.types.Update.de_json(json_string)
-    bot.process_new_updates([update])
-    return '!', 200
-
-
-@server.route('/')
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url=APP_URL)
-    return '!', 200
-
-
-if __name__ == '__main__':
-    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port='80') 
