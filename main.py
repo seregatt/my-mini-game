2bc3209fa13abf2777832fe8c918357c6c5994de
unknown <tariel4ik@gmail.com>
2024-11-12 08:35:59 +0300

    Update for bot

--- main.py
+++ main.py
@@ -4,8 +4,27 @@ from flask import Flask, request
 
 TOKEN = '7580167697:AAGQhpoMI4Oq-WpulwP5uPMOz_edLlpWJ8Q'
 APP_URL = f'https://my-app-game-ef1ec4b42519.herokuapp.com/{TOKEN}'
-bot = telebot.Telebot(TOKEN)
+bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo(message):
+    bot.reply_to(message, message.text)
+
+
+@server.route('/' + TOKEN, methods=['POST'])
+def get_message():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return '!', 200
+
 
 @server.route('/')
 def webhook():
@@ -15,4 +34,4 @@ def webhook():
 
 
 if __name__ == '__main__':
-    server.run(host='0.0.0.0', post=int(os.environ.get('PORT', 5000)))
+    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
