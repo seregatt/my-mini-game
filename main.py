8caabbc5d708ed6eef559fe530c3a2b608b386c1
unknown <tariel4ik@gmail.com>
2024-11-12 17:41:23 +0300

    sample

--- main.py
+++ main.py
@@ -9,11 +9,14 @@ server = Flask(__name__)
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-   return render_template('index.html')
-if __name__ == '__main__':
-   app.run()
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run()
 
 @bot.message_handler(commands=['start'])
 def start(message):
