48053f962b7bde76685c5dc82302baf6662a9a33
unknown <tariel4ik@gmail.com>
2024-11-12 08:45:32 +0300

    Update

--- main.py
+++ main.py
@@ -10,12 +10,13 @@ server = Flask(__name__)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name.getPhotos())
 
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo(message):
     bot.reply_to(message, message.text)
+
 
 
 @server.route('/' + TOKEN, methods=['POST'])
